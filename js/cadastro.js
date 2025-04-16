196652203f6283662bacd268de019c09faf6b44f
Gabriel <gabrielvipeixoto7@gmail.com>
2025-04-16 03:13:29 -0300

    Inglobamento de todas as páginas feitas

---
+++ js/cadastro.js
@@ -0,0 +1,29 @@
+function togglePassword(inputId, iconId) {
+    const passwordInput = document.getElementById(inputId);
+    const eyeIcon = document.getElementById(iconId);
+    const iconPath = 'static/icons/';
+    if (passwordInput.type === 'password') {
+      passwordInput.type = 'text';
+      eyeIcon.querySelector('img').src = `${iconPath}eye-slash-solid.svg`;
+    } else {
+      passwordInput.type = 'password';
+      eyeIcon.querySelector('img').src = `${iconPath}eye-solid.svg`;
+    }
+  }
+  
+  function toggleEnter(inputId) {
+    const buttonCadastro = document.getElementById('button-cadastro');
+    const buttonEntrar = document.getElementById('button-entrar');
+  
+    if (inputId === 'form-cadastro') {
+      document.getElementById('form-login').style.display = 'none';
+      document.getElementById('form-cadastro').style.display = 'flex';
+      buttonCadastro.style.backgroundColor = 'var(--cor-bg)';
+      buttonEntrar.style.backgroundColor = 'var(--cor-bgEscuro)';
+    } else {
+      document.getElementById('form-cadastro').style.display = 'none';
+      document.getElementById('form-login').style.display = 'flex';
+      buttonEntrar.style.backgroundColor = 'var(--cor-bg)';
+      buttonCadastro.style.backgroundColor = 'var(--cor-bgEscuro)';
+    }
+  }
