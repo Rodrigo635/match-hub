7e2997fc0890ea97337bb7ea25f6f6aa5e593a60
Rodrigo <116903783+Rodrigo635@users.noreply.github.com>
2025-04-19 13:54:01 -0300

    Ajustes visuais gerais
    adicionado - Ajuste para a seção de jogos no index.js filtros e função de ver mais, ajustes no header, footer e páginas de cadastro e cadastro concluido.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
-let contador = 1;
-const total_Divs = document.querySelectorAll("[class*='visivel-']").length;
+let contador = 12;
+const total_Divs = document.querySelectorAll(".col-12.col-md-6.col-lg-3");
 const { btn_VerMais, btn_VerMenos } = PegarSelector(contador);
+
+for (let i = 12; i <= total_Divs.length; i++) {
+  // Adiciona a classe "visivel-i" a cada div
+  total_Divs[i].classList.add("d-none");
+}
 
 /* AUXILIARES */
 
 // Seleciona a div, o botão "Ver mais" e o botão "Ver menos" com base no contador
 function PegarSelector(contador) {
   return {
-    div_Visivel: document.querySelector(".visivel-" + contador),
     btn_VerMais: document.querySelector(".ver-mais"),
     btn_VerMenos: document.querySelector(".ver-menos"),
   };
@@ -32,86 +36,101 @@ function btnVerMaisDesativado() {
 
 // Exibir mais jogos
 function ExibirJogos() {
-  const div_Visivel = document.querySelector(".visivel-" + contador);
+  const {btn_VerMais, btn_VerMenos } = PegarSelector(contador);
 
-  if (div_Visivel.classList.contains("d-none")) {
-    contador++;
-    div_Visivel.classList.remove("d-none");
+  if (contador != total_Divs.length) {
+    for (let i = contador; i < contador + 8; i++) {
+      total_Divs[i].classList.remove("d-none");
+    }
+
+    contador += 8;
+
+    if(contador == total_Divs.length) {
+      btn_VerMais.classList.add("d-none");
+      btnVerMaisDesativado();
+    }
+  }
+
+  // Se o contador for maior que 1, mostra o botão "Ver menos"
+  if (contador > 12) {
     btn_VerMenos.classList.remove("d-none");
-
-    // Se o contador for maior que o total de divs, esconde o botão "Ver mais"
-    contador > total_Divs && btnVerMaisDesativado();
+    btn_VerMenos.style.cursor = "pointer";
   }
 }
 
 // Ocultar jogos
 function EsconderJogos() {
-  //Contador diminuido para acessar a ultima div visivel
-  contador--;
-  const div_Visivel = document.querySelector(".visivel-" + contador);
+  const {btn_VerMais, btn_VerMenos } = PegarSelector(contador);
 
-  //Pega a div atual e esconde ela
-  div_Visivel.classList.add("d-none");
-  btnVerMaisAtivo();
+  if (contador > 12) {
+    for (let i = contador - 8; i < contador; i++) {
+      total_Divs[i].classList.add("d-none");
+    }
 
-  // Se a lista de jogos estiver no inicio, esconde o botão "Ver menos"
-  if (contador == 1) {
+    contador -= 8;
+  }
+
+  // Se o contador for menor ou igual a 12, esconde o botão "Ver menos" e mostra o botão "Ver mais"
+  if (contador < total_Divs.length) {
     btn_VerMenos.classList.add("d-none");
+    btn_VerMais.classList.remove("d-none");
+    btnVerMaisAtivo();
   }
 }
 
 // Função principal para aplicar ambos os filtros
 function AplicarFiltros() {
+  // Seleciona todos os cards e os filtros
   const cards = document.querySelectorAll(".card");
   const jogo_Escolhido = document.getElementById("jogos").value.toLowerCase();
   const campeonato_Escolhido = document.getElementById("campeonatos").value.toLowerCase();
 
+   // Variável para verificar se algum filtro está ativo
   let filtroAtivo = false;
 
-  // Percorre todos os cards um por um e verifica se o título do jogo ou do campeonato corresponde ao filtro selecionado
+  // 
   cards.forEach((card) => {
     const tituloJogo = card.querySelector("h5").innerText.toLowerCase();
     const tituloCampeonato = card.querySelector("h6").innerText.toLowerCase();
-    let ExibirCard = true; // Variável para controlar a exibição do card
+    let ExibirCard = true;
 
+    // Verifica se o título do jogo contém o valor do filtro
     if (jogo_Escolhido !== "todos" && jogo_Escolhido !== "jogos") {
-      // Verifica se o título do jogo contém o texto selecionado
       ExibirCard = ExibirCard && tituloJogo.includes(jogo_Escolhido);
       filtroAtivo = true;
     }
 
-    /* True se aplicar filtro */
+    // Verifica se o título do campeonato contém o valor do filtro
     if (campeonato_Escolhido !== "todos" && campeonato_Escolhido !== "campeonatos") {
-      // Verifica se o título do campeonato contém o texto selecionado
       ExibirCard = ExibirCard && tituloCampeonato.includes(campeonato_Escolhido);
       filtroAtivo = true;
     }
 
-    // Se o card não corresponder a nenhum dos filtros, esconde o card
     card.parentElement.style.display = ExibirCard ? "block" : "none";
   });
 
-  // Se tiver filtro ativo, esconde o botão "Ver mais" e "Ver menos" e exibe os todos os cards filtrados
+  // Verifica se algum filtro está ativo e aplica a lógica de exibição
   if (filtroAtivo) {
     EsconderBtn();
-
-    // Percorre todas as divs e deixa todas visiveis
-    for (let i = 1; i <= total_Divs; i++) {
-      document.querySelector(".visivel-" + i).classList.remove("d-none");
+    
+    // Mostra todas as divs
+    for (let i = 0; i < total_Divs.length; i++) {
+      total_Divs[i].classList.remove("d-none");
     }
   }
-  // Volta ao estado inicial caso nao tenha filtro ativo
-  else {
+  // Se não houver filtro ativo, aplica a lógica de exibição padrão
+   else {
+    const { btn_VerMais, btn_VerMenos } = PegarSelector(contador);
     btn_VerMais.classList.remove("d-none");
     btnVerMaisAtivo();
 
-    // Esconde todas as divs, exceto a primeira
-    for (let i = 1; i <= total_Divs; i++) {
-      document.querySelector(".visivel-" + i).classList.add("d-none");
+    for (let i = 12; i < total_Divs.length; i++) {
+      total_Divs[i].classList.add("d-none");
     }
-    contador = 1;
+    contador = 12; 
   }
 }
+
 
 // Eventos que ocorre quando o usuario escolher um jogo ou campeonato
 document.getElementById("jogos").addEventListener("change", AplicarFiltros);
