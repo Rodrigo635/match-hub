b5d811680fd21c98445913d6dc806bfa29667f75
Luis Gustavo Fernandes Rivalta <luisg16fernandes@gmail.com>
2025-04-28 15:44:11 -0300

    Correção do salto da navbar
    Corrigi o "salto" que acontecia na navbar ao scrollar a pagina.

--- js/global.js
+++ js/global.js
@@ -2,29 +2,42 @@ let isAnimating = false; // Flag única para tudo
 const menuCollapse = document.querySelector("#mainNavbar");
 
 // --- Controle do Ícone ---
-function toggleMenuIcon() {    
-    const menuIcon = document.querySelector("#menu");
-    menuIcon.classList.toggle("active");
-    isAnimating = true;
+function toggleMenuIcon() {
+  const menuIcon = document.querySelector("#menu");
+  menuIcon.classList.toggle("active");
+  isAnimating = true;
 }
 
 // Quando o menu ABRE
 menuCollapse.addEventListener("show.bs.collapse", () => {
-    toggleMenuIcon();
+  toggleMenuIcon();
 });
 
 // Quando o menu FECHA
 menuCollapse.addEventListener("hide.bs.collapse", () => {
-    toggleMenuIcon();
+  toggleMenuIcon();
 });
 
-window.addEventListener('scroll', function() {
-    const menu = document.querySelector('nav')
-  
-    if (window.scrollY > 0) {
-      menu.classList.add('fixed');
-    } else {
-      menu.classList.remove('fixed');
+// --- Correção do "salto" ao scrollar ---
+window.addEventListener("scroll", function () {
+  const menu = document.querySelector("nav");
+  const menuHeight = menu.offsetHeight;
+  let spacer = document.querySelector("#spacer");
+
+  if (window.scrollY > 0) {
+    menu.classList.add("fixed");
+
+    if (!spacer) {
+      spacer = document.createElement("div");
+      spacer.id = "spacer";
+      spacer.style.height = menuHeight + "px";
+      menu.parentNode.insertBefore(spacer, menu);
     }
-  });
-  
+  } else {
+    menu.classList.remove("fixed");
+
+    if (spacer) {
+      spacer.remove();
+    }
+  }
+});
