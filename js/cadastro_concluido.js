b597f135af9960e8cb275bbfc2b81c3852c93742
Luis Gustavo Fernandes Rivalta <luisg16fernandes@gmail.com>
2025-04-29 16:18:29 -0300

    Troca dos gifs da pagina de cadastro concluido
    Foi adicionado gifs na pagina de cadastro concluído e foi utilizado o JS para randomizar o gif em cada cadastro feito.

---
+++ js/cadastro_concluido.js
@@ -0,0 +1,24 @@
+const gifs = [
+  "static/icons/sonic.gif",
+  "static/icons/caveira.gif",
+  "static/icons/cj.gif",
+  "static/icons/gato.gif",
+  "static/icons/link.gif",
+  "static/icons/mario.gif",
+  "static/icons/yasuo.gif",
+  "static/icons/cs.gif",
+  "static/icons/jett.gif",
+  "static/icons/toasty.gif",
+  "static/icons/clash.gif",
+];
+
+function carregarGifAleatorio() {
+  const gifEscolhido = gifs[Math.floor(Math.random() * gifs.length)];
+  const gifElement = document.getElementById("gif-concluido");
+  if (gifElement) {
+    gifElement.src = gifEscolhido;
+    gifElement.classList.add("loaded");
+  }
+}
+
+window.addEventListener("DOMContentLoaded", carregarGifAleatorio);
