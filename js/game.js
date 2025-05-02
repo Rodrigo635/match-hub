1cfa465799d382696a9f116d30408561e389924a
Miguel <miganjos88@gmail.com>
2025-05-02 10:45:22 -0300

    adicionando os cards

--- js/game.js
+++ js/game.js
@@ -1,8 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const raw = localStorage.getItem("selectedGame");
-  if (!raw) return console.error("Nenhum jogo em storage.");
+function carregarCampeonatos(game) {
+  if(!game.jogos) return;
 
-  const game = JSON.parse(raw);
+  const campeonato = document.getElementById("campeonato");
+  const time1 = document.getElementById("time1");
+  const time2 = document.getElementById("time2");
+  const data = document.getElementById("data");
+  const horario = document.getElementById("horario");
+  const link = document.getElementById("link-jogo");
+
+  game.jogos.forEach(element => {
+    campeonato.textContent = element.campeonato;
+    time1.textContent = element.time1;
+    time2.textContent = element.time2;
+    data.textContent = element.data;
+    horario.textContent = element.horario;
+    link.href = element.link;
+  });
+}
+
+function carregarJogo(game){
   document.getElementById("game-title").textContent      = game.game;
   document.getElementById("game-tournament").textContent = game.tournament;
 
@@ -15,5 +31,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const img = document.getElementById("game-image");
   img.style.display = "none";
 
+}
+
+function carregarPagina(){
+  const raw = localStorage.getItem("selectedGame");
+  if (!raw) return console.error("Nenhum jogo em storage.");
+
+  const game = JSON.parse(raw);
+
+
+  carregarCampeonatos(game);
+  carregarJogo(game);
+
+  
   localStorage.removeItem("selectedGame");
-});
+}
+
+
+document.addEventListener("DOMContentLoaded", carregarPagina);
