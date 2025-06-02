f088101e87c847d82f39380c60de6997ac9b662f
Miguel <miganjos88@gmail.com>
2025-06-02 19:34:58 -0300

    Adicionando Java Spring boot

---
+++ backend-match-hub/src/main/java/com/match_hub/backend_match_hub/dtos/UserDTO.java
@@ -0,0 +1,4 @@
+package com.match_hub.backend_match_hub.dtos;
+
+public record UserDTO(String username, String password, String email, String profilePicture) {
+}
---
+++ backend-match-hub/src/test/java/com/match_hub/backend_match_hub/BackendMatchHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.match_hub.backend_match_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMatchHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
