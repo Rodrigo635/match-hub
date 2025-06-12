5cb5f67c56046c0af58fd0acd5c3257af1dbce9c
Miguel <miganjos88@gmail.com>
2025-06-11 21:58:20 -0300

    adicionando classes USER

---
+++ backend-match-hub/src/main/java/com/match_hub/backend_match_hub/dtos/UserDTO.java
@@ -0,0 +1,30 @@
+package com.match_hub.backend_match_hub.dtos;
+
+import jakarta.validation.constraints.*;
+import java.time.LocalDate;
+
+import javax.management.relation.Role;
+
+public record UserDTO(
+
+    @NotBlank(message = "O nome de usuário é obrigatório")
+    String username,
+
+    @NotBlank(message = "A senha é obrigatória")
+    @Pattern(
+        regexp = "^(?=.*[A-Z])(?=.*[!@#$%^&*()_+\\-={}:;\"'<>?,./]).{8,}$",
+        message = "A senha deve conter no mínimo 8 caracteres, uma letra maiúscula e um caractere especial"
+    )
+    String password,
+
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "Formato de e-mail inválido")
+    String email,
+
+    @NotNull(message = "A data de nascimento é obrigatória")
+    @Past(message = "A data de nascimento deve estar no passado")
+    LocalDate birthDate,
+
+    String profilePicture
+
+) {}
---
+++ backend-match-hub/src/main/java/com/match_hub/backend_match_hub/enums/Role.java
@@ -0,0 +1,6 @@
+package com.match_hub.backend_match_hub.enums;
+
+public enum UserRole {
+    ADMIN,
+    USER
+}
