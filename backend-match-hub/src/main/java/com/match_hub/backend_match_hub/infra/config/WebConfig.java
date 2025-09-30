4e99c1c703ee2d3fb1f450402bae77e691c218f0
Michel Pereira <michelpereira4410@gmail.com>
2025-09-29 21:23:11 -0300

    fix: ajusta o cors

--- backend-match-hub/src/main/java/com/match_hub/backend_match_hub/infra/config/WebConfig.java
+++ backend-match-hub/src/main/java/com/match_hub/backend_match_hub/infra/config/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://match-hub.vercel.app")
+                .allowedOrigins("http://localhost:3000", "https://match-hub.vercel.app", "http://82.112.245.100:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("location", "Location")
