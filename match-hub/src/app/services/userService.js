88447f4f16c0cc06b14dc40eceea1eccaa72cf8d
Miguel <miganjos88@gmail.com>
2025-10-10 16:44:42 -0300

    fix: arruma avatar function

--- match-hub/src/app/services/userService.js
+++ match-hub/src/app/services/userService.js
@@ -8,7 +8,7 @@ import {
 } from "./globalService";
 
 const BASE_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/users`;
-
+const BASE_URL2 = `${process.env.NEXT_PUBLIC_BASE_URL}`;
 // --------------------- CRUD Usuários ---------------------
 export async function getUsers(page = 0, size = 5) {
   return await getData(page, size, BASE_URL);
@@ -153,6 +153,22 @@ export async function uploadProfileImage(id, formData, token) {
   return res;
 }
 
+export async function uploadPublicAvatar(selectedAvatar, token) {
+  const res = await fetch(`${BASE_URL}/api/user/avatar`, {
+    method: "PUT",
+    headers: { Authorization: `Bearer ${token}` },
+    body: JSON.stringify(selectedAvatar),
+    credentials: "include",
+  });
+
+  if (!res.ok) {
+    const text = await res.text();
+    throw new Error(`Erro ao fazer upload da imagem: ${res.status}, ${text}`);
+  }
+
+  return res;
+}
+
 export async function deleteProfileImage(id, token) {
   const res = await fetch(`${BASE_URL}/image/delete/${id}`, {
     method: "DELETE",
@@ -166,4 +182,17 @@ export async function deleteProfileImage(id, token) {
   }
 
   return res;
+}
+
+export async function getPublicAvatar(){
+  const res = await fetch(`${BASE_URL2}/avatar`, {
+    method: "GET",
+    credentials: "include",
+  });
+  if (!res.ok) {
+    const text = await res.text();
+    throw new Error(`Erro ao buscar avatar: ${res.status}, ${text}`);
+  }
+
+  return res.json();
 }
