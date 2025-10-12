66db7a45dd0eb240371d6725bdb3e7f6af27d939
Michel Pereira <michelpereira4410@gmail.com>
2025-10-12 11:31:57 -0300

    feat: adiciona string tournament

--- backend-match-hub/src/main/java/com/match_hub/backend_match_hub/dtos/user/favorites/FavoriteGamesDTO.java
+++ backend-match-hub/src/main/java/com/match_hub/backend_match_hub/dtos/user/favorites/FavoriteGamesDTO.java
@@ -2,6 +2,7 @@ package com.match_hub.backend_match_hub.dtos.user.favorites;
 
 public record FavoriteGamesDTO(
         Long id,
-        String name
+        String name,
+        String tournament
 ) {
 }
